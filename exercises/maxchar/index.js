78d3647ae60177ba77adc2297abfba0f53dd4784
Kenny Le <kennyle37@gmail.com>
2018-08-09 22:00:47 -0700

    Completed maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,42 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+/*
+set a storage
+set a result
+
+loop through the entire string
+  at each string, check if it currently exists in the object
+    if it does, +1
+  if not add it in
+
+for each of the property inside the object
+  if it is the highest one, set our storage to it
+
+return storage
+*/
+
+function maxChar(str) {
+  let result = {};
+  let storage = str[0];
+
+  for (let i = 0; i < str.length; i++) {
+    if (result[str[i]]) {
+      result[str[i]] += 1;
+    } else {
+      result[str[i]] = 1;
+    }
+  }
+
+  for (let key in result) {
+    //check to see if the current key is bigger than the storage
+      //if it is set the storage to the current key
+    if (result[key] > result[storage]) {
+      storage = key;
+    }
+  }
+  
+  return storage;
+}
 
 module.exports = maxChar;
