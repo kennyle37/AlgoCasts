0a77576271eed172a6046e5c3979173dd012bf16
Kenny Le <kennyle37@gmail.com>
2018-08-11 20:31:48 -0700

    Completed capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,30 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+/*
+split the string into arrays by spaces
+capitalize the first letter of every value in that array
+join them back by space
+*/
+
+/*
+split the string into arrays by spaces
+capitalize the first letter of every value in that array
+join them back by space
+*/
+// let arr = [ 'a', 'short', 'sentence' ];
+// arr[i][0]
+
+function capitalize(str) {
+  let strSpl = str.split(" ");
+  let word = '';
+
+  for (let i = 0; i < strSpl.length; i++) {
+    strSpl[i] = strSpl[i].charAt(0).toUpperCase() + strSpl[i].substr(1);
+    word += strSpl[i] + ' ';
+  }
+
+  return word.trim();
+}
 
 module.exports = capitalize;
