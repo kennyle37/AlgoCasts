23d464b67ed17a10f8310ed4e7d89d1c97927707
Kenny Le <kennyle37@gmail.com>
2018-08-14 13:59:41 -0700

    Complete pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,59 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// (4) => base 7
+// (5) => base 9
+// (6) => base 11
+
+//width is (n * 2) - 1
+//spread count is n - i
+//# count is width - (spread * 2)
+
+  /*
+  need the current total width
+  need the totalSpread
+  need the totalBlock
+  need the storage
+
+  iterate from 1 to n
+
+    while spread > 0
+      add '_' to storage
+      spread--
+    
+    while block > 0
+      add # to block
+      block--
+
+    console.log(storage)
+  */
+
+function pyramid(n) {
+  let width = (n * 2) - 1; //5
+
+  for (let i = 1; i <= n; i++) {
+    let storage = '';
+    let spread = n - i; // 2 =>  1 => 0
+    let revSpread = spread;
+    let block = width - (spread * 2);
+    
+    while (spread > 0) {
+      storage += ' ';
+      spread--;
+    }
+
+    while (block > 0) {
+      storage += '#';
+      block--;
+    }
+    
+    while (revSpread > 0) {
+      storage += ' ';
+      revSpread--;
+    }
+
+    console.log(storage)
+  }
+}
 
 module.exports = pyramid;
